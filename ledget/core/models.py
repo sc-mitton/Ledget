017e5bc522d35fa377c239155226203df705ef89
sc-mitton <smitton.byu@gmail.com>
2023-02-25 23:05:47 -0500

    working state

--- ledget/core/models.py
+++ ledget/core/models.py
@@ -1,26 +1,23 @@
 from django.db import models
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
     BaseUserManager
 )
-from rest_framework.authtoken.models import Token
 
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, name, password, **extra_fields):
+    def create_user(self, email, password, first_name='',
+                    last_name='', **extra_fields):
         """Create and save a new user."""
         if not email:
             raise ValueError('Users must have an email address.')
 
         user = self.model(
             email=self.normalize_email(email),
-            name=name,
+            first_name=first_name,
+            last_name=last_name,
             **extra_fields
         )
         user.set_password(password)
@@ -28,9 +25,11 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, name, password, **extra_fields):
+    def create_superuser(self, email, first_name, last_name,
+                         password, **extra_fields):
         """Create, save, and return a new superuser."""
-        user = self.create_user(email, name, password, **extra_fields)
+        user = self.create_user(email, first_name, last_name,
+                                password, **extra_fields)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -41,14 +40,15 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_Name = models.CharField(max_length=50, blank=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'phone']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
