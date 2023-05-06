3c814862dc5db0e1e593fef88a8d427a37776ee8
sc-mitton <smitton.byu@gmail.com>
2023-05-06 07:20:51 -0400

    updates

--- appfront/src/components/modals/FinicityConnect.js
+++ appfront/src/components/modals/FinicityConnect.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 import withModal from '../utils/withModal'
-
+import './FinicityConnect.css'
 
 const FinicityConnect = (props) => {
-    return (
+    const [loading, setLoading] = useState(true)
 
-        <div className="modal-footer">
-            <button
-                className="cancel-button"
-                onClick={() => props.cleanUp()}
-            >
-                cancel
-            </button>
-        </div>
+    return (
+        <>
+            <div id="connect-container">
+                <iframe seamless width='300px' height='600px'>
+                </iframe>
+            </div>
+            <div id="connect-footer">
+                <button
+                    className="cancel-button"
+                    onClick={() => props.setVisible(false) && props.cleanUp()}
+                >
+                    cancel
+                </button>
+            </div>
+        </>
     )
 }
 
