f574cae055702d8a60cd7f935b3617502b804396
sc-mitton <smitton.byu@gmail.com>
2023-08-05 19:34:23 -0600

    update

--- app/home/src/components/inputs/Schedule.jsx
+++ app/home/src/components/inputs/Schedule.jsx
@@ -12,7 +12,7 @@ import { useClickClose } from '@utils'
 
 
 const Button = React.forwardRef((props, ref) => {
-    const { onClick } = props
+    const { onClick, ...rest } = props
 
     return (
         <>
@@ -35,15 +35,16 @@ const Button = React.forwardRef((props, ref) => {
                 aria-haspopup="true"
                 aria-expanded={`${open}`}
                 aria-controls="schedule-dropdown"
+                {...rest}
             >
                 <Calendar
-                    width={'1.7em'}
-                    height={'1.7em'}
+                    width={'1.5em'}
+                    height={'1.5em'}
                     fill={'var(--input-placeholder2)'}
                 />
                 <Arrow
-                    width={'.9em'}
-                    height={'.9em'}
+                    width={'.8em'}
+                    height={'.8em'}
                     stroke={'var(--input-placeholder2)'}
                 />
             </div>
@@ -79,7 +80,8 @@ const ModeSelector = ({ mode, setMode }) => {
                             style={{
                                 color: selected
                                     ? 'var(--white-text)'
-                                    : 'var(--input-placeholder2)'
+                                    : 'var(--input-placeholder2)',
+                                fontWeight: '400'
                             }}
                         >
                             {option.label}
@@ -100,8 +102,8 @@ const DayPicker = ({ day, setDay, setOpen }) => {
         <td key={dayNumber}>
             <div
                 className={`day
-                    ${day === dayNumber ? 'selected' : ''}
-                    ${activeDay === dayNumber ? 'active' : ''}
+                    ${day === dayNumber ? 'selected-day' : ''}
+                    ${activeDay === dayNumber ? 'active-day' : ''}
                     `}
                 onClick={() => setDay(dayNumber)}
                 role="button"
@@ -206,138 +208,117 @@ const DayPicker = ({ day, setDay, setOpen }) => {
     )
 }
 
-const WeekOptions = ({ weekOptions }) => {
-    return (
-        weekOptions.map((option) => (
-            <ComboSelect.Option
-                value={option.value}
-                disabled={option.disabled}
-                key={option.value}
+const WeekPicker = (props) => {
+    const { week, setWeek, weekDay, setWeekDay, setOpen } = props
+    const [activeWeekNumber, setActiveWeekNumber] = useState(null)
+    const [activeWeekDay, setActiveWeekDay] = useState(null)
+    const ref = useRef(null)
+
+    useEffect(() => {
+        ref.current?.focus()
+    }, [])
+
+    const formatWeek = (weekNumber) => {
+        switch (weekNumber) {
+            case 1:
+                return `${weekNumber}st`
+            case 2:
+                return `${weekNumber}nd`
+            case 3:
+                return `${weekNumber}rd`
+            default:
+                return `${weekNumber}th`
+        }
+    }
+
+    const formatWeekDay = (weekDayNumber) => {
+        switch (weekDayNumber) {
+            case 0:
+                return 'Sunday'
+            case 1:
+                return 'Monday'
+            case 2:
+                return 'Tuesday'
+            case 3:
+                return 'Wednesday'
+            case 4:
+                return 'Thursday'
+            case 5:
+                return 'Friday'
+            case 6:
+                return 'Saturday'
+            default:
+                return ''
+        }
+    }
+
+    const WeekNumber = ({ weekNumber }) => (
+        <li>
+            <div
+                className={`week-number
+                    ${week === weekNumber ? 'selected-week-number' : ''}
+                    `}
+                onClick={() => setWeek(weekNumber)}
+                role="button"
+                aria-label={`Select week ${weekNumber + 1}`}
+                aria-pressed={`${week === weekNumber}`}
+                aria-selected={`${week === weekNumber}`}
+                tabIndex={-1}
             >
-                {({ active, selected }) => (
-                    <div className={
-                        `slct-item ${active && "a-slct-item"}
-                    ${selected && "s-slct-item"}`}
-                    >
-                        <div>{option.label}</div>
-                    </div>
-                )}
-            </ComboSelect.Option>
-        ))
+                {formatWeek(weekNumber)}
+            </div>
+        </li>
     )
-}
 
-const WeekSelector = ({ value, onChange }) => {
-    const [weekOptions, setWeekOptions] = useState([
-        { label: 'First', value: '1', disabled: false },
-        { label: 'Second', value: '2', disabled: false },
-        { label: 'Third', value: '3', disabled: false },
-        { label: 'Fourth', value: '4', disabled: false },
-        { label: 'Last', value: '-1', disabled: false },
-    ])
-
-    return (
-        <ComboSelect
-            name="schedule-week"
-            value={value}
-            onChange={onChange}
-        >
-            {({ open }) => (
-                <>
-                    <ComboSelect.Button
-                        className="btn-icon-r btn-rnd btn-pill"
-                        style={{ marginRight: '12px' }}
-                    >
-                        <span style={{ opacity: value ? '1' : '.5' }}>
-                            {value
-                                ? `${weekOptions.find((week) => week.value === value).label}`
-                                : 'Week'}
-                        </span>
-                        <Arrow
-                            width={'.8em'}
-                            height={'.8em'}
-                            stroke={value
-                                ? 'var(--main-text-gray)'
-                                : 'var(--input-placeholder2)'
-                            }
-                        />
-                    </ComboSelect.Button>
-                    <ComboSelect.Options className="select-container" static >
-                        <DropAnimation visible={open} className="dropdown select" >
-                            <WeekOptions weekOptions={weekOptions} />
-                        </DropAnimation>
-                    </ComboSelect.Options>
-                </>
-            )}
-        </ComboSelect>
-    )
-}
-
-const WeekdayOptions = ({ days }) => {
-    return (
-        days.map((option) => (
-            <ComboSelect.Option
-                value={option.value}
-                disabled={option.disabled}
-                key={option.value}
+    const WeekDay = ({ dayNumber }) => (
+        <li>
+            <div
+                className={`week-day
+                    ${weekDay === dayNumber ? 'selected-week-day' : ''}
+                    `}
+                onClick={() => setWeekDay(dayNumber)}
+                role="button"
+                aria-label={`Select day ${dayNumber + 1}`}
+                aria-pressed={`${weekDay === dayNumber}`}
+                aria-selected={`${weekDay === dayNumber}`}
+                tabIndex={-1}
             >
-                {({ active, selected }) => (
-                    <div className={
-                        `slct-item ${active && "a-slct-item"}
-                    ${selected && "s-slct-item"}`}
-                    >
-                        <div>{option.label}</div>
-                    </div>
-                )}
-            </ComboSelect.Option>
-        ))
+                {formatWeekDay(dayNumber)}
+            </div>
+        </li>
     )
-}
-
-const WeekdaySelector = ({ value, onChange }) => {
-    const [days, setDays] = useState([
-        { label: 'Monday', value: 'mon', disabled: false },
-        { label: 'Tuesday', value: 'tue', disabled: false },
-        { label: 'Wednesday', value: 'wed', disabled: false },
-        { label: 'Thursday', value: 'thu', disabled: false },
-        { label: 'Friday', value: 'fri', disabled: false },
-        { label: 'Saturday', value: 'sat', disabled: false },
-        { label: 'Sunday', value: 'sun', disabled: false },
-    ])
 
     return (
-        <ComboSelect
-            name="schedule-day"
-            value={value}
-            onChange={onChange}
+        <div
+            className="week-picker"
+            ref={ref}
+            onBlur={() => {
+                setActiveDay(null)
+                setFocused(false)
+            }}
+            onMouseEnter={() => {
+                setActiveWeekDay(null)
+                setActiveWeekNumber(null)
+            }}
+            onKeyDown={(event) => handleKeyDown(event)}
+            onFocus={() => setFocused(true)}
+            tabIndex={0}
         >
-            {({ open }) => (
-                <>
-                    <ComboSelect.Button
-                        className="btn-icon-r btn-rnd btn-pill"
-                    >
-                        <span style={{ opacity: value ? '1' : '.5' }}>
-                            {value
-                                ? `${days.find((day) => day.value === value).label}`
-                                : 'Day'}
-                        </span>
-                        <Arrow
-                            width={'.8em'}
-                            height={'.8em'}
-                            stroke={value.length > 0
-                                ? 'var(--main-text-gray)'
-                                : 'var(--input-placeholder2)'
-                            }
-                        />
-                    </ComboSelect.Button>
-                    <ComboSelect.Options className="select-container" static >
-                        <DropAnimation visible={open} className="dropdown select" >
-                            <WeekdayOptions days={days} />
-                        </DropAnimation>
-                    </ComboSelect.Options>
-                </>
-            )}
-        </ComboSelect>
+            <ul className="week-picker">
+                {
+                    Array.from({ length: 4 }, (_, i) =>
+                        <Week key={i} weekNumber={i + 1} />
+                    )
+                }
+            </ul>
+            <ul className="week-day-picker">
+                {
+                    Array.from({ length: 7 }, (_, i) =>
+                        <WeekDay key={i} dayNumber={i} />
+                    )
+                }
+            </ul>
+        </div>
     )
 }
 
@@ -363,7 +344,8 @@ const ScheduleSelector = (props) => {
         setVisible: setOpen
     })
 
-    // Clear other inputs different mode's inputs are entered
+    // Clear other inputs different
+    // mode's inputs are entered
     useEffect(() => {
         if (day) {
             setWeek('')
@@ -371,7 +353,8 @@ const ScheduleSelector = (props) => {
         }
     }, [day])
 
-    // Clear other inputs different mode's inputs are entered
+    // Clear other inputs different mode's
+    // inputs are entered
     useEffect(() => {
         if (week || weekDay) {
             setDay('')
@@ -391,6 +374,10 @@ const ScheduleSelector = (props) => {
             }
             <Button
                 onClick={() => setOpen(!open)}
+                onFocus={(e) => {
+                    e.stopPropagation()
+                    open && setOpen(false)
+                }}
                 ref={buttonRef}
             />
             <DropAnimation
@@ -416,21 +403,44 @@ const ScheduleSelector = (props) => {
                         />
                     }
                     {mode === 'week' && (
-                        <div id="week-selectors">
-                            <div className="btn-pill">
-                                <WeekSelector
-                                    value={week}
-                                    onChange={setWeek}
-                                />
-                            </div>
-                            <div className="btn-pill">
-                                <WeekdaySelector
-                                    value={weekDay}
-                                    onChange={setWeekDay}
-                                />
-                            </div>
-                        </div>
+                        <WeekPicker
+                            week={week}
+                            setWeek={setWeek}
+                            weekDay={weekDay}
+                            setWeekDay={setWeekDay}
+                            setOpen={setOpen}
+                        />
                     )}
+                </div>
+            </DropAnimation>
+        </>
+    )
+}
+
+const DatePicker = (props) => {
+    const [open, setOpen] = useState(false)
+    const ref = useRef(null)
+    const buttonRef = useRef(null)
+
+    useClickClose({
+        refs: [ref, buttonRef],
+        visible: open,
+        setVisible: setOpen
+    })
+
+    return (
+        <>
+            <Button
+                onClick={() => setOpen(!open)}
+                ref={buttonRef}
+            />
+            <DropAnimation
+                visible={open}
+                className="dropdown"
+                id="schedule-dropdown"
+            >
+                <div ref={ref}>
+
                 </div>
             </DropAnimation>
         </>
