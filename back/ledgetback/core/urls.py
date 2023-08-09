60c8d383c6599534e1f03f5457573dbb476db6d2
sc-mitton <smitton.byu@gmail.com>
2023-08-08 20:18:18 -0600

    updates

--- back/ledgetback/core/urls.py
+++ back/ledgetback/core/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views.user_view import UserView
-from .views.service_views import (
+from .views.user import UserView
+from .views.service import (
     PriceView,
     SubscriptionView,
     CustomerView
 )
-from .views.link_view import LinkView
+from .views.plaid_link import (
+     PlaidLinkTokenView,
+     PlaidTokenExchangeView
+)
 
 urlpatterns = [
     path('prices', PriceView.as_view(), name='prices'),
@@ -14,5 +17,8 @@ urlpatterns = [
          CustomerView.as_view(), name='customer'),
     path('user/<str:user_id>/subscription',
          SubscriptionView.as_view(), name='subscription'),
-    path('link', LinkView.as_view(), name='link'),
+    path('plaid_link_token', PlaidLinkTokenView.as_view(),
+         name='plaid_link_token'),
+    path('plaid_token_exchange', PlaidTokenExchangeView.as_view(),
+           name='plaid_token_exchange'),
 ]
