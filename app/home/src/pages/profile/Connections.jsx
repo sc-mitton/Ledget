415846d50cab7b20ee87a205af5cdde4794b5de4
sc-mitton <smitton.byu@gmail.com>
2023-08-16 20:56:57 -0600

    updates

--- app/home/src/pages/profile/Connections.jsx
+++ app/home/src/pages/profile/Connections.jsx
@@ -27,13 +27,16 @@ const ConnectionsContext = ({ children }) => {
     const [deleteQue, setDeleteQue] = useState([])
     const { data: user } = useGetMeQuery()
     const { data: plaidItems, isLoading: fetchingPlaidItems } = useGetPlaidItemsQuery(user.id)
+    const [editing, setEditing] = useState(false)
 
     const value = {
         deleteQue,
         setDeleteQue,
         plaidItems,
         fetchingPlaidItems,
-        user
+        user,
+        editing,
+        setEditing,
     }
     return (
         <DeleteContext.Provider value={value}>
@@ -71,20 +74,10 @@ const DeleteAllButton = ({ show, onClick }) => {
 }
 
 const DeleteButton = (props) => {
-    const { visible, account, ...rest } = props
-    const [hover, setHover] = useState(false)
+    const { visible, account, onClick, ...rest } = props
     const [loaded, setLoaded] = useState(false)
     const [deleteClass, setDeleteClass] = useState('')
     const { deleteQue, setDeleteQue } = useContext(DeleteContext)
-
-    const springProps = useSpring({
-        transformOrigin: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'relative',
-        transform: hover ? 'rotate(45deg)' : 'rotate(0deg)',
-    })
 
     useEffect(() => {
         setLoaded(true)
@@ -97,69 +90,95 @@ const DeleteButton = (props) => {
 
     return (
         <>
-            <animated.button
-                style={springProps}
+            <button
                 className={`btn delete-button ${deleteClass}`}
                 aria-label="Remove account"
                 disabled={!visible}
-                onMouseEnter={() => setHover(true)}
-                onMouseLeave={() => setHover(false)}
                 {...rest}
                 onClick={(e) => {
                     e.preventDefault()
+                    onClick()
                     if (deleteQue.filter((item) => item.account === account.id).length === 0) {
                         setDeleteQue((prev) => [...prev, { account: account.id }])
                     }
                 }}
             >
                 <span><Delete /></span>
-            </animated.button>
+            </button>
         </>
+    )
+}
+
+const typeMap = {
+    'checking': 'Checking',
+    'savings': 'Savings',
+    'credit card': 'Credit Card',
+    'cd': 'CD',
+    'money market': 'Money Market',
+    'ira': 'IRA',
+    '401k': '401k',
+    'student': 'Student Loan',
+    'mortgage': 'Mortgage',
+}
+
+const Account = (props) => {
+    const { account, showDelete } = props
+    const [removed, setRemoved] = useState(false)
+    const { editing } = useContext(DeleteContext)
+
+    useEffect(() => {
+        !editing && setRemoved(false)
+    }, [editing])
+
+    const springs = useSpring({
+        to: {
+            opacity: editing && removed ? 0 : 1,
+            maxHeight: editing && removed ? '0px' : '100px',
+            visibility: editing && removed ? 'hidden' : 'visible',
+            marginTop: editing && removed ? '0px' : '8px',
+            marginBottom: editing && removed ? '0px' : '8px',
+            width: '100%',
+        }
+    })
+
+    return (
+        <animated.div className={'account-name'} style={springs}>
+            <div>
+                <span>
+                    {account.name}
+                </span>
+                <DeleteButton
+                    visible={showDelete}
+                    account={account}
+                    onClick={() => setRemoved(true)}
+                />
+            </div >
+            <div>
+                <span>
+                    {typeMap[account.subtype]}
+                    &nbsp;&nbsp;
+                    &nbsp;&bull;&nbsp;&bull;&nbsp;&bull;&nbsp;&bull;&nbsp;
+                    {account.mask}
+                </span>
+            </div>
+        </animated.div>
     )
 }
 
 const AccountsColumn = ({ item, showDelete, start, end }) => {
 
-    const typeMap = {
-        'checking': 'Checking',
-        'savings': 'Savings',
-        'credit card': 'Credit Card',
-        'cd': 'CD',
-        'money market': 'Money Market',
-        'ira': 'IRA',
-        '401k': '401k',
-        'student': 'Student Loan',
-        'mortgage': 'Mortgage',
-    }
-
     return (
         <div>
             {item.accounts.slice(start, end).map((account) => (
-                <div className={'account'}>
-                    <div className="account-name">
-                        <div>
-                            <span>
-                                {account.name}
-                            </span>
-                            <DeleteButton visible={showDelete} account={account} />
-                        </div >
-                        <div>
-                            <span>
-                                {typeMap[account.subtype]}
-                                &nbsp;&nbsp;
-                                &nbsp;&bull;&nbsp;&bull;&nbsp;&bull;&nbsp;&bull;&nbsp;
-                                {account.mask}
-                            </span>
-                        </div>
-                    </div>
-                </div>
+                <Account key={account.id} account={account} showDelete={showDelete} />
             ))}
         </div>
     )
 }
 
-const PlaidItem = ({ item, edit }) => {
+const PlaidItem = ({ item }) => {
     const { setDeleteQue } = useContext(DeleteContext)
+    const { editing } = useContext(DeleteContext)
 
     return (
         <div className="institution">
@@ -175,7 +194,7 @@ const PlaidItem = ({ item, edit }) => {
                 </div>
                 <div>
                     <DeleteAllButton
-                        show={edit}
+                        show={editing}
                         onClick={() => setDeleteQue(
                             (prev) => [...prev, { item: item.id }]
                         )}
@@ -187,41 +206,45 @@ const PlaidItem = ({ item, edit }) => {
                     item={item}
                     start={0}
                     end={item.accounts.length / 2 + 1}
-                    showDelete={edit}
+                    showDelete={editing}
                 />
                 <AccountsColumn
                     item={item}
                     start={item.accounts.length / 2 + 1}
                     end={item.accounts.length}
-                    showDelete={edit}
+                    showDelete={editing}
                 />
             </div>
         </div >
     )
 }
 
-const Header = ({ edit, setEdit, onPlus }) => (
-    <div className="header">
-        <h1>Connections</h1>
-        <div className='header-btns'>
-            {!edit &&
+const Header = ({ onPlus }) => {
+    const { editing, setEditing } = useContext(DeleteContext)
+
+    return (
+        <div className="header">
+            <h1>Connections</h1>
+            <div className='header-btns'>
+                {!editing &&
+                    <button
+                        className="btn-clr btn"
+                        onClick={() => setEditing(!editing)}
+                        aria-label="Edit institution connections"
+                    >
+                        <Edit />
+                    </button>}
                 <button
                     className="btn-clr btn"
-                    onClick={() => setEdit(!edit)}
-                    aria-label="Edit institution connections"
+                    onClick={onPlus}
+                    aria-label="Add institution connection"
                 >
-                    <Edit />
-                </button>}
-            <button
-                className="btn-clr btn"
-                onClick={onPlus}
-                aria-label="Add institution connection"
-            >
-                <Plus />
-            </button>
+                    <Plus />
+                </button>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 const Inputs = () => {
     const { deleteQue } = useContext(DeleteContext)
@@ -243,12 +266,13 @@ const Inputs = () => {
 }
 
 const Connections = () => {
-    const [edit, setEdit] = useState(false)
     const [saving, setSaving] = useState(false)
     const {
         plaidItems,
         fetchingPlaidItems,
-        user
+        user,
+        editing,
+        setEditing,
     } = useContext(DeleteContext)
 
     const { data: plaidToken, refetch: refetchPlaidToken } = useGetPlaidTokenQuery()
@@ -276,9 +300,9 @@ const Connections = () => {
         token: plaidToken?.link_token,
         ...(isOauth ? { receivedRedirectUri: window.location.href } : {}),
     }
-    if (import.meta.env.VITE_PLAID_REDIRECT_URI) {
-        config.receivedRedirectUri = import.meta.env.VITE_PLAID_REDIRECT_URI
-    }
+    // if (import.meta.env.VITE_PLAID_REDIRECT_URI) {
+    //     config.receivedRedirectUri = import.meta.env.VITE_PLAID_REDIRECT_URI
+    // }
     const { open, exit, ready } = usePlaidLink(config)
 
     // 30 min timeout to refresh token
@@ -294,28 +318,20 @@ const Connections = () => {
             <LoadingShimmer visible={fetchingPlaidItems} />
             {!fetchingPlaidItems &&
                 <div id="connections-page">
-                    <Header
-                        edit={edit}
-                        setEdit={setEdit}
-                        onPlus={() => open()}
-                    />
+                    <Header onPlus={() => open()} />
                     <ShadowedContainer id="accounts-list">
                         <div>
                             {plaidItems?.map((item) => (
-                                <PlaidItem
-                                    edit={edit}
-                                    key={item.id}
-                                    item={item}
-                                />
+                                <PlaidItem key={item.id} item={item} />
                             ))}
                         </div>
                     </ShadowedContainer>
                     <div className="footer-container">
-                        {edit && <form>
+                        {editing && <form>
                             <Inputs />
                             <SubmitForm
                                 submitting={saving}
-                                onCancel={() => setEdit(false)}
+                                onCancel={() => setEditing(false)}
                             />
                         </form>}
                     </div>
