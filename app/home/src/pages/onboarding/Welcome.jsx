10b685bd184395c5c2d08536fe680e65bcb342d8
sc-mitton <smitton.byu@gmail.com>
2023-08-21 10:39:36 -0600

    update

--- app/home/src/pages/onboarding/Welcome.jsx
+++ app/home/src/pages/onboarding/Welcome.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import './styles/Welcome.css'
+import Plus from '@assets/icons/Plus'
 
 const Welcome = () => {
+
     return (
-        <div>Welcome</div>
+        <div className="window2 onboarding">
+            <h1 className="spaced-header">Getting Started</h1>
+            <h3 className="spaced-header2">
+                Welcome to Ledget!
+            </h3>
+            <div className="body">
+                We're excited to have you here.
+                Let's get started by connecting your financial accounts.
+            </div>
+            <div className="btn-container action">
+                <button
+                    className="btn-grn btn3 btn-icon-r"
+                    id="connect-account-btn"
+                >
+                    <span>Link Account</span>
+                    <Plus
+                        className='animated-stroke'
+                        width={'1em'}
+                        height={'1em'}
+                    />
+                </button>
+            </div>
+        </div>
     )
 }
 
