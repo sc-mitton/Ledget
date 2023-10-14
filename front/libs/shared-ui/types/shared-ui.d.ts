525b37c5ed40d99c4480659a4b12c93199c11ef1
sc-mitton <smitton.byu@gmail.com>
2023-10-13 20:39:09 -0600

    from shared.* libs since they've been migrated to ts

--- front/libs/shared-ui/types/shared-ui.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'shared-ui' {
-    export type ToastType = 'success' | 'error' | 'warning' | 'info';
-
-    export type ToastItem = {
-        id: number;
-        message: string;
-        type: ToastType;
-    }
-}
--- front/libs/shared-utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as withSmallModal } from './src/hocs/withSmallModal'
-export { default as withModal } from './src/hocs/withModal'
-export { default as useAccessEsc } from './src/hooks/useAccessEsc'
-export * from './src/funcs'
