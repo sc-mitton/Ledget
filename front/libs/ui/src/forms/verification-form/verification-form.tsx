69c8ec20d182bc55c8a9e37eb8f0f30109d13aa6
sc-mitton <smitton.byu@gmail.com>
2023-11-13 10:50:54 -0700

    verification form buttons swap

--- front/libs/ui/src/forms/verification-form/verification-form.tsx
+++ front/libs/ui/src/forms/verification-form/verification-form.tsx
@@ -3,7 +3,7 @@ import './verification-form.css';
 import { useState, useEffect, useRef } from 'react'
 
 import './verification-form.css'
-import { GreenSubmitButton, ResendButton } from '../../buttons/buttons'
+import { LightGrnWideButton, ResendButton } from '../../buttons/buttons'
 import { Otc } from '../../inputs/otc/otc'
 
 interface VerificationFormProps {
@@ -49,7 +49,7 @@ export const VerificationForm = (props: VerificationFormProps) => {
           <Otc codeLength={6} />
           <input type="hidden" name="csrf_token" value={flow?.csrf_token} />
           <input type="hidden" name="email" value={identifier} />
-          <GreenSubmitButton
+          <LightGrnWideButton
             type="submit"
             name="method"
             value="code"
@@ -58,7 +58,7 @@ export const VerificationForm = (props: VerificationFormProps) => {
             submitting={hasInitialSent && submitting}
           >
             Submit
-          </GreenSubmitButton>
+          </LightGrnWideButton>
         </form>
         <form
           action={flow?.ui.action}
