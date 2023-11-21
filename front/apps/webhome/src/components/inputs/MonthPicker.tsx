36f35f44e6ec1ea39dd095b4e432609364c02af0
sc-mitton <smitton.byu@gmail.com>
2023-11-21 12:23:48 -0700

    updates

--- front/apps/webhome/src/components/inputs/MonthPicker.tsx
+++ front/apps/webhome/src/components/inputs/MonthPicker.tsx
@@ -56,7 +56,7 @@ const MonthPicker = () => {
                     newDateOptions[year] = [month]
                 }
             }
-
+            setDateOptions(newDateOptions)
         }
     }, [userIsFetched, user])
 
