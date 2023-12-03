19f6bfa71f456d71488e8e26f76a48fa2cdca16b
sc-mitton <smitton.byu@gmail.com>
2023-12-03 09:52:53 -0700

    updates

--- front/apps/webportal/src/forms/Login.jsx
+++ front/apps/webportal/src/forms/Login.jsx
@@ -288,7 +288,7 @@ const Login = () => {
         <AnimatePresence mode="wait">
             {!email && !searchParams.get('mfa')
                 ?
-                <SlideMotionDiv className='window' key="initial" position={flow ? 'first' : 'last'}>
+                <SlideMotionDiv className='window' key="initial" position={flow ? 'first' : 'fixed'}>
                     <EmailForm setEmail={setEmail} flow={flow} socialSubmit={submit} />
                     <div className="below-login--container">
                         <LinkArrowButton
