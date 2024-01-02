0844c9af125b55a6fe11d8712789ee61e1634d9c
sc-mitton <scm.mitton@gmail.com>
2024-01-01 23:55:08 -0700

    updates

--- back/ledgetback/budget/tests/data.py
+++ back/ledgetback/budget/tests/data.py
@@ -30,34 +30,55 @@ multiple_category_creation_payload = [
 reminders = Reminder.objects.all()
 number_of_reminders = reminders.count()
 
-single_bill_creation_payload = {
-    'name': 'Test Bill',
-    'emoji': '🤑',
-    'period': 'month',
-    'day': 1,
-    'lower_amount': 1000,
-    'upper_amount': 10000,
-}
 if number_of_reminders > 0:
-    single_bill_creation_payload['reminders'] = [
-        {'id': str(reminder.id)
-         for reminder in reminders[:number_of_reminders // 2]}]
-
-
-multiple_bill_creation_payload = [
-    {
-        'name': f'Test Bill {i}',
+    single_bill_creation_payload = {
+        'name': 'Test Bill',
         'emoji': '🤑',
         'period': 'month',
         'day': 1,
         'lower_amount': 1000,
         'upper_amount': 10000,
+        'reminders': [
+            {'id': str(reminder.id)
+                for reminder in reminders[:number_of_reminders // 2]}
+        ]
     }
-    for i in range(1, 20)
-]
+else:
+    single_bill_creation_payload = {
+        'name': 'Test Bill',
+        'emoji': '🤑',
+        'period': 'month',
+        'day': 1,
+        'lower_amount': 1000,
+        'upper_amount': 10000
+    }
+
 
 if number_of_reminders > 0:
-    for i in range(len(multiple_bill_creation_payload)):
-        multiple_bill_creation_payload[i]['reminders'] = [
-            {'id': str(reminder.id)
-             for reminder in reminders[:number_of_reminders // 2]}]
+    multiple_bill_creation_payload = [
+        {
+            'name': f'Test Bill {i}',
+            'emoji': '🤑',
+            'period': 'month',
+            'day': 1,
+            'lower_amount': 1000,
+            'upper_amount': 10000,
+            'reminders': [
+                {'id': str(reminder.id)
+                    for reminder in reminders[:number_of_reminders // 2]}
+            ]
+        }
+        for i in range(1, 20)
+    ]
+else:
+    multiple_bill_creation_payload = [
+        {
+            'name': f'Test Bill {i}',
+            'emoji': '🤑',
+            'period': 'month',
+            'day': 1,
+            'lower_amount': 1000,
+            'upper_amount': 10000,
+        }
+        for i in range(1, 20)
+    ]
