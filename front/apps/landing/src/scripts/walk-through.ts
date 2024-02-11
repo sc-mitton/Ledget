1e6d6cf24d673605c8967070e43b677d649d8791
sc-mitton <scm.mitton@gmail.com>
2024-02-11 01:28:41 -0700

    updates

--- front/apps/landing/src/scripts/walk-through.ts
+++ front/apps/landing/src/scripts/walk-through.ts
@@ -4,21 +4,58 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 function animate() {
-    gsap.fromTo('.progress--line',
-        { right: '100%' },
-        {
-            scrollTrigger: {
-                trigger: '#walk-through',
-                start: 'top bottom',
-                end: 'bottom bottom',
-                scrub: true,
-                pin: true,
-                toggleActions: 'play none none none'
-            },
-            right: '0%',
-            duration: 1
-        }
-    )
+
+    var tl = gsap.timeline()
+
+    var circles = gsap.utils.toArray('.progress--circle') as HTMLDivElement[];
+    const firstCircle = circles[0];
+    const lastCircle = circles[circles.length - 1];
+    const middleCircle = circles[Math.floor(circles.length / 2)];
+
+
+    tl.to(firstCircle, {
+        scrollTrigger: {
+            trigger: '.progress',
+            start: 'top 50%',
+            end: 'top 50%',
+            scrub: true,
+        },
+        backgroundColor: 'var(--blue-sat)',
+    }).to('.progress--top-bar', {
+        scrollTrigger: {
+            trigger: '.progress',
+            start: 'top 50%',
+            end: 'center 50%',
+            scrub: true,
+        },
+        bottom: '50%',
+    }).to(
+        middleCircle, {
+        scrollTrigger: {
+            trigger: '.progress',
+            start: 'center 50%',
+            end: 'center 50%',
+            scrub: true,
+        },
+        backgroundColor: 'var(--blue-sat)',
+    }).to('.progress--bottom-bar', {
+        scrollTrigger: {
+            trigger: '.progress',
+            start: 'center 50%',
+            end: 'bottom 50%',
+            scrub: true,
+        },
+        bottom: '0%',
+    }).to(lastCircle, {
+        scrollTrigger: {
+            trigger: '.progress',
+            start: 'bottom 50%',
+            end: 'bottom 50%',
+            scrub: true,
+        },
+        backgroundColor: 'var(--blue-sat)',
+    });
+
 }
 
 animate();
