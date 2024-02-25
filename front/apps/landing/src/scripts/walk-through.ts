4bf388fe9bf7cdd5736068aecefd39db1607fa14
sc-mitton <scm.mitton@gmail.com>
2024-02-25 11:46:42 -0700

    update

--- front/apps/landing/src/scripts/walk-through.ts
+++ front/apps/landing/src/scripts/walk-through.ts
@@ -1,27 +1,15 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { MotionPathPlugin } from 'gsap/all';
+import { CustomEase } from 'gsap/CustomEase';
 
 gsap.registerPlugin(MotionPathPlugin);
 gsap.registerPlugin(ScrollTrigger);
-
-const motionPath1 = [
-    { x: 5, y: -60 },
-    { x: 0, y: -120 },
-    { x: -5, y: -180 },
-    { x: 0, y: -240 }
-]
-
-const motionPath2 = [
-    { x: -5, y: -60 },
-    { x: 0, y: -120 },
-    { x: 5, y: -180 },
-    { x: 0, y: -240 }
-]
+gsap.registerPlugin(CustomEase);
 
 function animate() {
     var circles = gsap.utils.toArray('.progress--circle') as HTMLDivElement[];
-    var text = gsap.utils.toArray('.step') as HTMLHeadingElement[];
+    var text = gsap.utils.toArray('.steps--step') as HTMLHeadingElement[];
     // Query all emoji (#emoji-number) and convert to array
     var numberOfEmojis = gsap.utils.toArray('.emoji').length;
 
@@ -29,31 +17,45 @@ function animate() {
         scrollTrigger: {
             trigger: '#product-walk-through',
             start: 'top 90%',
-            toggleActions: 'play none none none',
-        }
+            toggleActions: 'restart none none none',
+        },
+        repeatDelay: 4,
     })
 
-    tl.to(circles[0], { backgroundColor: 'var(--blue)' })
+    tl.to(circles[0], { backgroundColor: 'var(--blue-sat)' })
         .to('.progress--top-bar', { bottom: '50%', delay: 0 }, 'start')
-        .to(circles[1], { backgroundColor: 'var(--blue)' })
-        .to('.progress--bottom-bar', { bottom: '.5em', delay: .5 }, 'middle')
-        .to(circles[2], { backgroundColor: 'var(--blue)' })
+        .to(circles[1], { backgroundColor: 'var(--blue-sat)' })
+        .fromTo('.progress--bottom-bar', { bottom: '50%' }, { bottom: '.5em', delay: 0 }, 'middle')
+        .to(circles[2], { backgroundColor: 'var(--blue-sat)' })
 
     tl.fromTo(text[0], { opacity: .2 }, { opacity: 1, duration: .2 }, 'start')
         .fromTo(text[1], { opacity: .2 }, { opacity: 1, duration: .2 }, 'middle')
         .fromTo(text[2], { opacity: .2 }, { opacity: 1, duration: .2 }, 'end')
 
-
+    const duration = 2;
+    const stagger = .1
     for (let i = 0; i < numberOfEmojis; i++) {
         tl.to(`#emoji-${i}`, {
-            scale: window.innerWidth < 768 ? .75 : 1,
-            motionPath: { path: Math.random() > .5 ? motionPath1 : motionPath2, align: 'self' },
-            duration: 7,
+            scale: window.innerWidth < 768 ? .5 : 1,
+            y: -200 + Math.random() * -40,
+            duration: duration,
+            delay: i * stagger,
+        }, 'start')
+
+        tl.to(`#emoji-${i}`, {
             opacity: 0,
-            delay: i,
-            repeat: -1,
+            duration: duration * .5,
+            delay: i * stagger + duration * .5,
         }, 'start')
     }
+
+    const dollars = gsap.utils.toArray('.dollar') as HTMLDivElement[];
+    tl.from(dollars, {
+        scale: 0,
+        duration: .6,
+        stagger: .1,
+        ease: CustomEase.create("custom", "M0,0 C0.11,0.494 0.144,0.975 0.328,1.065 0.455,1.126 0.504,1 1,1 "),
+    }, 'end-=30%')
 }
 
 animate();
