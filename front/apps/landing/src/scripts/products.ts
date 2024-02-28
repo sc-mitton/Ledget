0bfead8a1a34812440562f8137924028ebd73f8a
sc-mitton <scm.mitton@gmail.com>
2024-02-27 21:13:10 -0700

    updates

--- front/apps/landing/src/scripts/products.ts
+++ front/apps/landing/src/scripts/products.ts
@@ -10,13 +10,13 @@ function animateProducts() {
             if (self.scroll() === 0 && self.direction === -1) {
                 gsap.to('.phone', {
                     y: Math.max(Math.min(self.getVelocity() / 100, -10), -40),
-                    duration: Math.min(Math.max(-1 * self.getVelocity() / 5000, .1), .3),
+                    duration: Math.min(Math.max(-1 * self.getVelocity() / 5000, .15), .3),
                     yoyo: true,
                     repeat: 1,
                 })
                 gsap.to('.web', {
                     y: Math.max(Math.min(self.getVelocity() / 120, -5), -30),
-                    duration: Math.min(Math.max(-1 * self.getVelocity() / 5500, .1), .2),
+                    duration: Math.min(Math.max(-1 * self.getVelocity() / 5500, .15), .2),
                     yoyo: true,
                     repeat: 1,
                 })
