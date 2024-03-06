bd380da8bfb75b5d8f836a456659aef134fcc1d5
sc-mitton <scm.mitton@gmail.com>
2024-03-06 00:40:14 -0700

    remove BIRD_SIGNING_KEY

--- back/ledgetback/core/views/user.py
+++ back/ledgetback/core/views/user.py
@@ -1,13 +1,10 @@
 
 from rest_framework.generics import RetrieveUpdateAPIView, GenericAPIView
-from django.conf import settings
 from rest_framework.response import Response
 from rest_framework import status
 
 from core.serializers import UserSerializer, EmailSerializer
 from core.permissions import IsAuthenticated
-
-BIRD_SIGNING_KEY = settings.BIRD_SIGNING_KEY
 
 
 class UserView(RetrieveUpdateAPIView):
