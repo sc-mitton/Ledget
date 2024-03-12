7a26fbd051a8106a20fc70e6ae08fe5581df1837
sc-mitton <scm.mitton@gmail.com>
2024-03-11 23:10:28 -0600

    add lodash external options for webhome

--- front/apps/webhome/vite.config.ts
+++ front/apps/webhome/vite.config.ts
@@ -64,4 +64,10 @@ export default defineConfig({
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
+
+  build: {
+    rollupOptions: {
+      external: ['lodash.clamp', 'lodash.startcase', 'lodash.tolower'],
+    },
+  }
 });
