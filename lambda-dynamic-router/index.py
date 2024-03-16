96b9b7c7a6c6d4fe94e88091cea2c57a2e2b1c82
sc-mitton <scm.mitton@gmail.com>
2024-03-16 14:01:47 -0600

    deploy new lambda func

--- lambda-dynamic-router/index.py
+++ lambda-dynamic-router/index.py
@@ -1,18 +1,22 @@
 def lambda_handler(event, context):
     request = event['Records'][0]['cf']['request']
     uri = request['uri']
+    host = request['headers']['host'][0]['value']
     origin = request['origin']
+
+    accounts_host = 'accounts.ledget.app'
+    main_host = 'ledget.app'
 
     VERSION = 'v1'
     default_origin = f'landing.{VERSION}.s3.amazonaws.com'
     accounts_app_origin = f'accounts.ledget.app.{VERSION}.s3.amazonaws.com'
     main_app_origin = f'app.ledget.{VERSION}.s3.amazonaws.com'
 
-    if uri.endswith('accounts.ledget.app'):
+    if host == accounts_host:
         # Route to the accounts bucket
         origin['s3']['domainName'] = \
             origin['s3']['domainName'].replace(default_origin, accounts_app_origin)
-    elif uri.endswith('ledget.app'):
+    elif host == main_host and uri:
         pass
     else:
         # Route to the app bucket
