499ce3f81c9736aadeb2c9a4a919c0ec258710af
sc-mitton <scm.mitton@gmail.com>
2024-03-16 10:16:47 -0600

    try and fix lambda function

--- lambda-dynamic-router/index.py
+++ lambda-dynamic-router/index.py
@@ -7,22 +7,23 @@ def lambda_handler(event, context):
     origin = request['origin']
 
     VERSION = 'v1'
+    default_domain = f'landing.{VERSION}'
     accounts_app_domain = f'accounts.ledget.app.{VERSION}'
     app_domain = f'app.ledget.{VERSION}'
-    landing_domain = f'landing.{VERSION}'
 
     logging.info(f'original URI: {uri}')
     logging.info(f'original origin: {origin}')
 
     if uri.endswith('accounts.ledget.app'):
         # Route to the accounts bucket
-        origin['s3']['domainName'] = f'{accounts_app_domain}.s3.amazonaws.com'
+        origin['s3']['domainName'] = \
+            origin['s3']['domainName'].replace(default_domain, accounts_app_domain)
     elif uri.endswith('ledget.app'):
-        # Route to the landing page bucket
-        origin['s3']['domainName'] = f'{landing_domain}.s3.amazonaws.com'
+        pass
     else:
         # Route to the app bucket
-        origin['s3']['domainName'] = f'{app_domain}.s3.amazonaws.com'
+        origin['s3']['domainName'] = \
+            origin['s3']['domainName'].replace(default_domain, app_domain)
 
     request['origin'] = origin
     return request
