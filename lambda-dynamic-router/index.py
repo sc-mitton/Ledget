9908a4ed91771c6c355e8ae2fee12461d586a908
sc-mitton <scm.mitton@gmail.com>
2024-03-16 13:45:22 -0600

    update lambda func

--- lambda-dynamic-router/index.py
+++ lambda-dynamic-router/index.py
@@ -1,29 +1,23 @@
-import logging
-
-
 def lambda_handler(event, context):
     request = event['Records'][0]['cf']['request']
     uri = request['uri']
     origin = request['origin']
 
     VERSION = 'v1'
-    default_domain = f'landing.{VERSION}'
-    accounts_app_domain = f'accounts.ledget.app.{VERSION}'
-    app_domain = f'app.ledget.{VERSION}'
-
-    logging.info(f'original URI: {uri}')
-    logging.info(f'original origin: {origin}')
+    default_origin = f'landing.{VERSION}.s3.amazonaws.com'
+    accounts_app_origin = f'accounts.ledget.app.{VERSION}.s3.amazonaws.com'
+    main_app_origin = f'app.ledget.{VERSION}.s3.amazonaws.com'
 
     if uri.endswith('accounts.ledget.app'):
         # Route to the accounts bucket
         origin['s3']['domainName'] = \
-            origin['s3']['domainName'].replace(default_domain, accounts_app_domain)
+            origin['s3']['domainName'].replace(default_origin, accounts_app_origin)
     elif uri.endswith('ledget.app'):
         pass
     else:
         # Route to the app bucket
         origin['s3']['domainName'] = \
-            origin['s3']['domainName'].replace(default_domain, app_domain)
+            origin['s3']['domainName'].replace(default_origin, main_app_origin)
 
     request['origin'] = origin
     return request
