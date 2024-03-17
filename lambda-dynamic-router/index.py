e68475ac5bbaf087c66053d4a47991d8d0629164
sc-mitton <scm.mitton@gmail.com>
2024-03-17 00:47:32 -0600

    update lambda func

--- lambda-dynamic-router/index.py
+++ lambda-dynamic-router/index.py
@@ -1,27 +1,15 @@
 def lambda_handler(event, context):
     request = event['Records'][0]['cf']['request']
     uri = request['uri']
-    host = request['headers']['host'][0]['value']
     origin = request['origin']
 
-    accounts_host = 'accounts.ledget.app'
-    main_host = 'ledget.app'
-
     default_origin = 'ledget-landing.s3.amazonaws.com'
-    accounts_app_origin = 'accounts.ledget.app.s3.amazonaws.com'
     main_app_origin = 'app.ledget.s3.amazonaws.com'
 
-    print(f'Host: {host}, URI: {uri}')
-    print(f'event: {event}')
-
-    if host == accounts_host:
-        # Route to the accounts bucket
-        origin['s3']['domainName'] = \
-            origin['s3']['domainName'].replace(default_origin, accounts_app_origin)
-    elif host == main_host and uri:
-        # Route to the app bucket
-        origin['s3']['domainName'] = \
-            origin['s3']['domainName'].replace(default_origin, main_app_origin)
+    if uri == 'index.html' or uri == 'favicon.ico':
+        origin = default_origin
+    else:
+        origin = main_app_origin
 
     request['origin'] = origin
     return request
