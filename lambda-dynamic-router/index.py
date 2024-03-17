5f80e828236994c093b384aa3254b22dac346e7d
sc-mitton <scm.mitton@gmail.com>
2024-03-17 10:57:55 -0600

    change lambda func

--- lambda-dynamic-router/index.py
+++ lambda-dynamic-router/index.py
@@ -3,13 +3,15 @@ def lambda_handler(event, context):
     uri = request['uri']
     origin = request['origin']
 
-    default_origin = 'ledget-landing.s3.amazonaws.com'
-    main_app_origin = 'app.ledget.s3.amazonaws.com'
+    # default_origin = 'ledget-landing.s3.amazonaws.com'
+    # main_app_origin = 'app.ledget.s3.amazonaws.com'
 
-    if uri == 'index.html' or uri == 'favicon.ico':
-        origin = default_origin
-    else:
-        origin = main_app_origin
+    print('uri: ' + uri)
+
+    # if uri == 'index.html' or uri == 'favicon.ico':
+    #     origin = default_origin
+    # else:
+    #     origin = main_app_origin
 
     request['origin'] = origin
     return request
