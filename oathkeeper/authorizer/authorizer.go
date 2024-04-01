4099900fe46e10620eace028c85e8c4b63153314
sc-mitton <scm.mitton@gmail.com>
2024-03-31 18:57:45 -0600

    update oathkeeper lambda func

--- oathkeeper/authorizer/authorizer.go
+++ oathkeeper/authorizer/authorizer.go
@@ -2,115 +2,21 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"os/exec"
-	"path/filepath"
+	"net/http/httptest"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
-
-	sdk "github.com/ory/oathkeeper-client-go"
+	"github.com/ory/oathkeeper/driver"
+	"github.com/ory/x/logrusx"
+	"github.com/spf13/pflag"
 )
 
-func getCachedJwks() (map[string]interface{}, error) {
-	secretName := "oathkeeper_jwks"
-
-	var (
-		secretCache, _ = secretcache.New()
-	)
-	result, err := secretCache.GetSecretString(secretName)
-	if err != nil {
-		return nil, err
-	}
-
-	var jwks map[string]interface{}
-	if err := json.Unmarshal([]byte(result), &jwks); err != nil {
-		return nil, err
-	}
-
-	return jwks, nil
-}
-
-func getStoredJwks() (map[string]interface{}, error) {
-	secretName := "oathkeeper_jwks"
-	secretRegion := os.Getenv("AWS_REGION")
-	if os.Getenv("ENVIRONMENT") == "test" {
-		secretName = "oathkeeper_jwks_test"
-	}
-	if secretRegion == "" {
-		secretRegion = "us-west-2"
-	}
-
-	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(secretRegion))
-	if err != nil {
-		log.Fatal(err)
-	}
-	svc := secretsmanager.NewFromConfig(config)
-	input := &secretsmanager.GetSecretValueInput{
-		SecretId:     aws.String(secretName),
-		VersionStage: aws.String("AWSCURRENT"),
-	}
-
-	result, err := svc.GetSecretValue(context.TODO(), input)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	var secretString string = *result.SecretString
-	var jwks map[string]interface{}
-	if err := json.Unmarshal([]byte(secretString), &jwks); err != nil {
-		return nil, err
-	}
-
-	return jwks, nil
-}
-
-func getJwksData() ([]byte, error) {
-	cachedJwks, err := getCachedJwks()
-	if err != nil || cachedJwks == nil {
-		fmt.Println("Failed to get cached JWKS. Getting from Secrets Manager...")
-		storedJwks, err := getStoredJwks()
-		if err != nil {
-			return nil, err
-		}
-		return json.Marshal(storedJwks)
-	}
-
-	jwksData, err := json.Marshal(cachedJwks)
-	if err != nil {
-		return nil, err
-	}
-
-	return jwksData, nil
-}
-
-func getDecisionsRequest(event events.APIGatewayProxyRequest) sdk.ApiApiDecisionsRequest {
-	configuration := sdk.NewConfiguration()
-
-	// Set the headers needed by oathkeeper
-	configuration.AddDefaultHeader("X-Forwarded-Method", event.HTTPMethod)
-	configuration.AddDefaultHeader("X-Forwarded-Uri", event.Path)
-	configuration.AddDefaultHeader("X-Forwarded-Host", event.Headers["Host"])
-	configuration.AddDefaultHeader("X-Forwarded-Proto", "http")
-	configuration.AddDefaultHeader("X-Forwarded-For", event.RequestContext.Identity.SourceIP)
-	configuration.AddDefaultHeader("Authorization", event.Headers["Authorization"])
-
-	configuration.Scheme = "http"
-	configuration.Host = "localhost:4456"
-
-	apiClient := sdk.NewAPIClient(configuration)
-	request := apiClient.ApiApi.Decisions(context.Background())
-
-	return request
-}
+var (
+	version = "0.4.0"
+	build   = "unknown"
+	date    = "unknown"
+)
 
 func generatePolicy(principalID string, effect string, resource string) events.APIGatewayCustomAuthorizerResponse {
 	authResponse := events.APIGatewayCustomAuthorizerResponse{
@@ -130,11 +36,11 @@ func generatePolicy(principalID string, effect string, resource string) events.A
 	return authResponse
 }
 
-func generateAllow(principalID string, resource string, oathkeeperResponse *http.Response) events.APIGatewayCustomAuthorizerResponse {
+func generateAllow(principalID string, resource string, decision *http.Response) events.APIGatewayCustomAuthorizerResponse {
 	policy := generatePolicy(principalID, "Allow", resource)
 	policy.Context = map[string]interface{}{
-		"authorizer": oathkeeperResponse.Header.Get("Authorization"),
-		"x-user":     oathkeeperResponse.Header.Get("X-User"),
+		"authorizer": decision.Header.Get("Authorization"),
+		"x-user":     decision.Header.Get("X-User"),
 	}
 	return policy
 }
@@ -143,43 +49,42 @@ func generateDeny(principalID string, resource string) events.APIGatewayCustomAu
 	return generatePolicy(principalID, "Deny", resource)
 }
 
-func init() {
-	// Get JWKS from AWS Secrets Manager
-	jwksData, err := getJwksData()
+func getDecision(event events.APIGatewayProxyRequest) (*http.Response, error) {
+
+	okFlags := pflag.NewFlagSet("ok", pflag.ContinueOnError)
+	okFlags.String("config", "./config.yml", "Path to the config file")
+	logger := logrusx.New("ORY Oathkeeper", version)
+
+	d := driver.NewDefaultDriver(logger, version, build, date, okFlags)
+	d.Registry().Init()
+
+	req, err := http.NewRequest(event.HTTPMethod, event.Path, nil)
+	req.Header.Add("X-Forwarded-For", event.RequestContext.Identity.SourceIP)
+	req.Header.Add("X-Forwarded-Proto", event.RequestContext.Protocol)
+	req.Header.Add("X-Forwarded-Host", event.RequestContext.DomainName)
+	req.Header.Add("X-Forwarded-Uri", event.Path)
+	req.Header.Add("X-Forwarded-For", event.RequestContext.Identity.SourceIP)
+	req.Header.Add("Cookie", event.Headers["Cookie"])
+
 	if err != nil {
-		fmt.Printf("Failed to get JWKS data: %v\n", err)
-		return
+		return nil, err
 	}
 
-	// Write JWKS to file
-	jwksFilePath := "tmp/jwks.json"
-	os.MkdirAll(filepath.Dir(jwksFilePath), 0755)
-	jwksFile, err := os.Create(jwksFilePath)
-	if err != nil {
-		fmt.Printf("Failed to create JWKS file: %v\n", err)
-		return
-	}
-	jwksFile.Write(jwksData)
-	jwksFile.Close()
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	rw := httptest.NewRecorder()
+	d.Registry().DecisionHandler().ServeHTTP(rw, req, next)
+
+	return rw.Result(), nil
 }
 
 func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	// Spawn Oathkeeper Server
-	cmd := exec.Command("oathkeeper", "--serve", "--config", "/etc/config/oathkeeper/config.yml")
-	if err := cmd.Start(); err != nil {
-		fmt.Printf("Error starting Oathkeeper server: %v\n", err)
+	decision, err := getDecision(event)
+
+	if err != nil {
+		return generateDeny("user", event.Path), err
+	} else {
+		return generateAllow("user", event.Path, decision), nil
 	}
-
-	fmt.Printf("Event: %v\n", event)
-	fmt.Printf("Context: %v\n", ctx)
-
-	resp, err := getDecisionsRequest(event).Execute()
-	if resp == nil || err != nil {
-		fmt.Printf("Error executing request: %v\n", err)
-		return generateDeny("user", event.RequestContext.Path), nil
-	}
-
-	return generateAllow("user", event.RequestContext.Path, resp), nil
 }
 
 func main() {
