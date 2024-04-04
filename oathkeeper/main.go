f75a9d07eb60ad6c647d0cfe4cf79eadded2dfa4
sc-mitton <scm.mitton@gmail.com>
2024-04-03 23:46:09 -0600

    updates

--- oathkeeper/main.go
+++ oathkeeper/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -10,10 +12,25 @@ func init() {
 	fmt.Println("init")
 }
 
-func Handle() {
+func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+
 	fmt.Println("Hello, World!")
+
+	return events.APIGatewayCustomAuthorizerResponse{
+		PrincipalID: "user",
+		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   "Allow",
+					Resource: []string{"*"},
+				},
+			},
+		},
+	}, nil
 }
 
 func main() {
-	lambda.Start(Handle)
+	lambda.Start(HandleRequest)
 }
