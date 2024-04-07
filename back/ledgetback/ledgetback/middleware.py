ee3560b9c2185c95f7221e284a491f271f80780c
sc-mitton <scm.mitton@gmail.com>
2024-04-06 22:23:53 -0600

    updates

--- back/ledgetback/ledgetback/middleware.py
+++ back/ledgetback/ledgetback/middleware.py
@@ -162,10 +162,14 @@ class OryAuthenticationMiddleware(MiddlewareMixin):
         """Validate the token against the JWK from Oathkeeper and
         return it if valid. Otherwise, return None."""
 
+        logger.info(
+            f"OATHKEEPER_PUBLIC_KEY is not null: {OATHKEEPER_PUBLIC_KEY is not None}")
+
         decoded_token = decode(
             token,
             key=OATHKEEPER_PUBLIC_KEY,
             algorithms=['RS256'],
+            options={'verify_exp': True}
         )
 
         return decoded_token
