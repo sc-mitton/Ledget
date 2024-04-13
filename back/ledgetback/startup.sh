03914fa88fbfbc2393f4027b8195e639e7274e67
sc-mitton <scm.mitton@gmail.com>
2024-04-13 15:39:15 -0600

    add envionrment variables to startup script

--- back/ledgetback/startup.sh
+++ back/ledgetback/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export DJANGO_SETTINGS_MODULE="ledgetback.settings.prod"
+export CELERY_BROKER_URL=" "
+export CELERY_RESULT_BACKEND=" "
 
 python manage.py wait_for_db &&
 python manage.py migrate &&
