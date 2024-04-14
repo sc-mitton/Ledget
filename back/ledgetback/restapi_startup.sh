8019a6cecf46d65640a67f3b2bc25fc7fdbf37fc
sc-mitton <scm.mitton@gmail.com>
2024-04-14 16:06:18 -0600

    updates

--- back/ledgetback/restapi_startup.sh
+++ back/ledgetback/restapi_startup.sh
@@ -2,13 +2,15 @@
 
 source set_env.sh
 
+# Run the server
+if [ $ENVIRONMENT == "dev" ]; then
+    python manage.py runserver &
+else
+    gunicorn -c gunicorn.conf.py ledgetback.wsgi &
+fi
+
+# Wait 20s for the server to start
+sleep 20
 python manage.py wait_for_db &&
 python manage.py makemigrations &&
 python manage.py migrate
-
-# Run the server
-if [ $ENVIRONMENT == "dev" ]; then
-    python manage.py runserver
-else
-    gunicorn -c gunicorn.conf.py ledgetback.wsgi
-fi
