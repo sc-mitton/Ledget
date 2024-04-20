7c5090d1137a1bdc4b50deb99e6c1df8280af22a
sc-mitton <scm.mitton@gmail.com>
2024-04-20 13:01:46 -0600

    update eb environment to ledget-restapi-prod

--- back/restapi/restapi/settings/prod.py
+++ back/restapi/restapi/settings/prod.py
@@ -8,7 +8,7 @@ from .get_aws_secret import get_secret
 ALLOWED_HOSTS = [
     'ledget.app',
     '*.ledget.app',
-    'restapi-prod-8b2lfj8.us-west-2.elasticbeanstalk.com',
+    'ledget-restapi-prod-o3pfbas.us-west-2.elasticbeanstalk.com',
 ]
 DOMAIN_URL = "https://api.ledget.app/"
 DOMAIN = 'api.ledget.app'
