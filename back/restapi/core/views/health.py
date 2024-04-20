1f7b614a9d5ae14a4fad7e918ea47af3e5532322
sc-mitton <scm.mitton@gmail.com>
2024-04-20 09:48:38 -0600

    fix lint error

--- back/restapi/core/views/health.py
+++ back/restapi/core/views/health.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 # Health view
 class HealthView(APIView):
     def get(self, request):
