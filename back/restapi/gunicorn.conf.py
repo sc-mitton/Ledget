be8994902877fe776855c57dcc4e3fda93963d99
sc-mitton <scm.mitton@gmail.com>
2024-04-20 15:36:52 -0600

    updates

--- back/restapi/gunicorn.conf.py
+++ back/restapi/gunicorn.conf.py
@@ -7,8 +7,5 @@ timeout = 10
 log_file = "-"
 workers = multiprocessing.cpu_count() * 2 + 1
 
-certfile = "/etc/pki/tls/certs/public.crt"
-keyfile = "/etc/pki/tls/certs/privatekey.pem"
-
 bind = "0.0.0.0:8000"
 module = "restapi.wsgi:application"
