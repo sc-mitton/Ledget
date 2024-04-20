a2d1645f6372ae7eade8654d36a1bca505e03e6d
sc-mitton <scm.mitton@gmail.com>
2024-04-20 09:56:59 -0600

    update gunicorn conf file to use https

--- back/restapi/gunicorn.conf.py
+++ back/restapi/gunicorn.conf.py
@@ -7,5 +7,8 @@ timeout = 10
 log_file = "-"
 workers = multiprocessing.cpu_count() * 2 + 1
 
+certfile = "/etc/pki/tls/certs/server.crt"
+keyfile = "/etc/pki/tls/private/server.key"
+
 bind = "0.0.0.0:8000"
 module = "restapi.wsgi:application"
