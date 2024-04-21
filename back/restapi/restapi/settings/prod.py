dfcb752397ab9ddf65c06a42aecd513948c2e058
sc-mitton <scm.mitton@gmail.com>
2024-04-20 22:58:14 -0600

    limit subnet options for debugging

--- back/restapi/restapi/settings/prod.py
+++ back/restapi/restapi/settings/prod.py
@@ -8,7 +8,7 @@ from .get_aws_secret import get_secret
 ALLOWED_HOSTS = [
     'ledget.app',
     '*.ledget.app',
-    'ledget-restapi-prod-4lylqdh.us-west-2.elasticbeanstalk.com',
+    'ledget-restapi-prod-uzd46hs.us-west-2.elasticbeanstalk.com',
 ]
 DOMAIN_URL = "https://api.ledget.app/"
 DOMAIN = 'api.ledget.app'
