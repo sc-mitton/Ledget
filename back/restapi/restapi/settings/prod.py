52399a84058f0e03db2598f2456eef1e5190d07d
sc-mitton <scm.mitton@gmail.com>
2024-05-01 07:40:48 -0600

    update beanstalk host

--- back/restapi/restapi/settings/prod.py
+++ back/restapi/restapi/settings/prod.py
@@ -8,7 +8,7 @@ from .get_aws_secret import get_secret
 ALLOWED_HOSTS = [
     'ledget.app',
     '*.ledget.app',
-    'ledget-restapi-prod-rly58e3.us-west-2.elasticbeanstalk.com',
+    'ledget-restapi-prod-yfz2dog.us-west-2.elasticbeanstalk.com',
 ]
 DOMAIN_URL = "https://api.ledget.app/"
 DOMAIN = 'api.ledget.app'
