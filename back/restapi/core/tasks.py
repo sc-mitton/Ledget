626335ccffb83d1f91e533d72cdee9a1e2c1f55a
sc-mitton <scm.mitton@gmail.com>
2024-05-16 08:27:07 -0600

    refactor delete user tasks and add task for cleaning up hanging users

--- back/restapi/core/tasks.py
+++ back/restapi/core/tasks.py
@@ -5,6 +5,7 @@ from celery import shared_task, group
 import ory_client.exceptions
 from plaid.model.item_remove_request import ItemRemoveRequest
 from django.conf import settings
+from django.db import OperationalError
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.db import transaction
@@ -51,20 +52,38 @@ def delete_ory_identity(user_id: str):
             raise e
 
 
-@shared_task
+@shared_task(
+    auto_retry_for=(OperationalError), retry_backoff=10, retry_jitter=True,
+    retry_kwargs={'max_retries': 3}
+)
+def update_db(user, plaid_items):
+
+    @transaction.atomic
+    def update():
+        try:
+            user.is_active = False
+            user.account.canceled_on = timezone.now()
+            user.save()
+            plaid_items.delete()
+        except Exception as e:
+            logger.error(f'Failed to update db for user {user.id} on cancelation: {e}')
+
+    update()
+
+
 def cancelation_cleanup(user_id: str) -> None:
     '''
     Deletes all third party data for a user. This is called when a user
     cancels their subscription and their billing cycle ends.
     '''
+    from financials.models import PlaidItem
+
     try:
         user = get_user_model().objects.get(id=user_id)
     except get_user_model().DoesNotExist:
         return
 
-    from financials.models import PlaidItem
     plaid_items = PlaidItem.objects.filter(user_id=user_id)
-
     delete_tasks = []
     for item in plaid_items:
         delete_task = delete_plaid_item.si(item.id, item.access_token)
@@ -74,16 +93,33 @@ def cancelation_cleanup(user_id: str) -> None:
         grouped_delete_tasks = group(delete_tasks)
         grouped_delete_tasks()
 
-    @transaction.atomic
-    def _update_db():
-        try:
-            user.is_active = False
-            user.account.canceled_on = timezone.now()
-            user.save()
-            plaid_items.delete()
-        except Exception as e:
-            logger.error(f'Failed to update db for user {user_id} on cancelation: {e}')
-
-    _update_db()
-
+    update_db.delay(user, plaid_items)
     delete_ory_identity.delay(user_id)
+
+
+@shared_task(auto_retry_for=(OperationalError), retry_backoff=10, retry_jitter=True,
+             retry_kwargs={'max_retries': 3})
+def cleanup_hanging_ory_users(user_id: str) -> None:
+    '''
+    When a user is adding anonther user to their account, there's a possibility
+    that the user might get added, but the linked user never finishes the process.
+    We don't want to leave those users hanging for security purposes.
+    '''
+
+    with ory_client.ApiClient(ory_configuration) as api_client:
+        api_instance = identity_api.IdentityApi(api_client)
+
+        identity = None
+        try:
+            identity = api_instance.get_identity(user_id)
+        except ory_client.exceptions.NotFoundException:
+            pass
+
+        # User is hanging
+        try:
+            if not identity or not identity['traits']['name']['first']:
+                get_user_model().objects.filter(id=user_id).delete()
+                api_instance.delete_identity(id=user_id)
+        except ory_client.exceptions.ApiException as e:
+            logger.error(f"Failed to delete hanging ory user {user_id}: {e}")
+            pass
