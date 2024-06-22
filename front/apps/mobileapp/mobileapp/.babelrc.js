013482896fee0db77651e32d2c0473ad71cb6b7a
sc-mitton <scm.mitton@gmail.com>
2024-06-22 12:09:08 -0700

    minor updates

--- front/apps/mobileapp/mobileapp/.babelrc.js
+++ front/apps/mobileapp/mobileapp/.babelrc.js
@@ -1,15 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
-        alias: {
-        },
-      },
-    ],
+    presets: ['babel-preset-expo']
   };
 };
