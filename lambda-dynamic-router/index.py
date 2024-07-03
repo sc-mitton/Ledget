23904978e7a25c8b02f0914f57fc973a65d426f4
sc-mitton <scm.mitton@gmail.com>
2024-07-03 11:52:05 -0600

    update dynamic router to auto add index.html

--- lambda-dynamic-router/index.py
+++ lambda-dynamic-router/index.py
@@ -1,23 +1,28 @@
 import re
 
+DEFAULT_ORIGIN = 'ledget-landing.s3.amazonaws.com'
+MAIN_APP_ORIGIN = 'app.ledget.s3.amazonaws.com'
+
 
 def lambda_handler(event, context):
     request = event['Records'][0]['cf']['request']
     uri = request['uri']
     origin = request['origin']
 
-    default_origin = 'ledget-landing.s3.amazonaws.com'
-    main_app_origin = 'app.ledget.s3.amazonaws.com'
-
+    # Check if should be redirected to main app
     should_redirect_checks = [
-        origin['s3']['domainName'] == default_origin,
-        re.match(r'\/[a-zA-Z]+', uri),
+        origin['s3']['domainName'] == DEFAULT_ORIGIN,
+        re.match(r'\/[a-zA-Z\/]+', uri),
         'privacy' not in uri,
         'terms' not in uri,
     ]
     if all(should_redirect_checks):
-        origin['s3']['domainName'] = main_app_origin
+        origin['s3']['domainName'] = MAIN_APP_ORIGIN
+        request['origin'] = origin
 
-    request['origin'] = origin
-    print('request: ', request)
+    # For the landing page, automatically add index.html to the uri
+    # if navigating to subfolder
+    if origin['s3']['domainName'] == DEFAULT_ORIGIN and re.match(r'\/[a-zA-Z\/]+', uri):
+        request['uri'] = f'{uri}/index.html'
+
     return request
