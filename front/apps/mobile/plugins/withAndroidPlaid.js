40c401f4b81fd8dced92626122f52a3e86c76998
sc-mitton <scm.mitton@gmail.com>
2024-08-04 01:35:02 -0600

    update android and ios plaid config plugins

--- front/apps/mobile/plugins/withAndroidPlaid.js
+++ front/apps/mobile/plugins/withAndroidPlaid.js
@@ -1,49 +1,45 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config_plugins_1 = require("expo/config-plugins");
-function applyPackage(mainApplication) {
-    var plaidPackageImport = "import com.plaid.PlaidPackage\n";
-    var plaidAddPackage = "packages.add(new PlaidPackage())";
-    // Make sure the project does not have the settings already
-    if (!mainApplication.includes(plaidPackageImport)) {
-        mainApplication = mainApplication.replace(/package com.ledget.mobileapp/, "package com.ledget.mobileapp\n".concat(plaidPackageImport));
-    }
-    if (!mainApplication.includes(plaidAddPackage)) {
-        mainApplication = mainApplication.replace(/return PackageList(this).packages/, "\n    ".concat(plaidAddPackage, "\n    return packages\n    "));
-    }
-    return mainApplication;
-}
+const { withAppBuildGradle, withSettingsGradle } = require('@expo/config-plugins')
+
 function applyImplementation(appBuildGradle) {
-    var plaidImplementation = "implementation project(':react-native-plaid-link-sdk')";
-    console.log(appBuildGradle);
+    const plaidImplementation = `implementation project(':react-native-plaid-link-sdk')`;
+
     // Make sure the project does not have the dependency already
     if (!appBuildGradle.includes(plaidImplementation)) {
-        return appBuildGradle.replace(/dependencies\s?{/, "dependencies {\n    ".concat(plaidImplementation));
+        return appBuildGradle.replace(
+            /dependencies\s?{/,
+            `dependencies {
+    ${plaidImplementation}`
+        );
     }
+
     return appBuildGradle;
 }
+
 function applySettings(gradleSettings) {
-    var plaidSettings = "include ':react-native-plaid-link-sdk'" + '\n' +
-        "project(':react-native-plaid-link-sdk').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-plaid-link-sdk/android')";
+    const plaidSettings = `include ':react-native-plaid-link-sdk'` + '\n' +
+        `project(':react-native-plaid-link-sdk').projectDir = new File(rootProject.projectDir, '../../../node_modules/react-native-plaid-link-sdk/android')`
+
     // Make sure the project does not have the settings already
-    if (!gradleSettings.includes("include ':react-native-plaid-link-sdk'")) {
-        return gradleSettings + plaidSettings;
+    if (!gradleSettings.includes(`include ':react-native-plaid-link-sdk'`)) {
+        return gradleSettings + plaidSettings
     }
-    return gradleSettings;
+
+    return gradleSettings
 }
-var withAndroidPlaid = function (expoConfig) {
-    expoConfig = (0, config_plugins_1.withMainApplication)(expoConfig, function (config) {
-        config.modResults.contents = applyPackage(config.modResults.contents);
-        return config;
-    });
-    expoConfig = (0, config_plugins_1.withAppBuildGradle)(expoConfig, function (config) {
-        config.modResults.contents = applyImplementation(config.modResults.contents);
-        return config;
-    });
-    expoConfig = (0, config_plugins_1.withSettingsGradle)(expoConfig, function (config) {
-        config.modResults.contents = applySettings(config.modResults.contents);
-        return config;
-    });
-    return expoConfig;
-};
-exports.default = withAndroidPlaid;
+
+const withAndroidPlaid = (expoConfig) => {
+
+    expoConfig = withAppBuildGradle(expoConfig, (config) => {
+        config.modResults.contents = applyImplementation(config.modResults.contents)
+        return config
+    })
+
+    expoConfig = withSettingsGradle(expoConfig, (config) => {
+        config.modResults.contents = applySettings(config.modResults.contents)
+        return config
+    })
+
+    return expoConfig
+}
+
+module.exports = withAndroidPlaid
---
+++ front/apps/mobile/plugins/withIOSPlaid.js
@@ -0,0 +1,28 @@
+const { withPodfile } = require('@expo/config-plugins')
+
+function applyPlaidVersion(appPodFile) {
+    const plaidImplementation = `pod 'Plaid', '~> 5.6.0'`;
+    const deploymentTarget = '14.0';
+
+    if (!appPodFile.includes(plaidImplementation)) {
+        appPodFile = appPodFile.replace(
+            /target\s'Ledget'\sdo/,
+            `target 'Ledget' do\n  ${plaidImplementation}`
+        );
+    };
+
+    const r = /(platform.*:ios.*)'[0-9.]+'/;
+    appPodFile = appPodFile.replace(r, `$1'${deploymentTarget}'`);
+    return appPodFile;
+}
+
+const withAndroidPlaid = (expoConfig) => {
+    expoConfig = withPodfile(expoConfig, (config) => {
+        config.modResults.contents = applyPlaidVersion(config.modResults.contents)
+        return config
+    })
+
+    return expoConfig
+}
+
+module.exports = withAndroidPlaid
