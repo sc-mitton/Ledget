ac2abab4444f03f416b3889ac2d5d31c4e49a35f
sc-mitton <scm.mitton@gmail.com>
2024-09-05 16:19:27 -0600

    add some code to chevron touchable

--- front/libs/native-ui/src/buttons/chevron-touchable/ChevronTouchable.tsx
+++ front/libs/native-ui/src/buttons/chevron-touchable/ChevronTouchable.tsx
@@ -1,22 +1,21 @@
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, TouchableOpacityProps, ViewStyle } from "react-native";
 
 import styles from './styles';
 import { Icon } from "../../restyled/Icon";
 import type { IconProps } from "../../restyled/Icon";
 import { ChevronDown, ChevronRight } from "geist-native-icons";
 
-interface Props {
+interface Props extends TouchableOpacityProps {
   direction?: 'right' | 'down';
-  onPress?: () => void;
-  children?: React.ReactNode;
   iconOverride?: React.ComponentType<IconProps>;
+  iconStyle?: ViewStyle;
 }
 
-export const ChevronTouchable = ({ direction = 'right', onPress, children, iconOverride: OverRide }: Props) => {
+export const ChevronTouchable = ({ direction = 'right', children, iconStyle, iconOverride: OverRide, style, ...rest }: Props) => {
   return (
-    <TouchableOpacity style={styles.container} onPress={onPress} activeOpacity={0.6}>
+    <TouchableOpacity style={[styles.container, style]} activeOpacity={0.6} {...rest}>
       {children}
-      <View style={styles.chevron}>
+      <View style={[styles.chevron, iconStyle]}>
         <Icon icon={direction === 'right' ? OverRide || ChevronRight : OverRide || ChevronDown} color='quinaryText' />
       </View>
     </TouchableOpacity>
