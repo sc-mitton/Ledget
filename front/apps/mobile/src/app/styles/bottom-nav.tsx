2bb30f4bd9d03177b8db20cde7f8d7902cd3e6a2
sc-mitton <scm.mitton@gmail.com>
2024-09-16 16:38:55 -0600

    add different spacing for bottom nav on android

--- front/apps/mobile/src/app/styles/bottom-nav.tsx
+++ front/apps/mobile/src/app/styles/bottom-nav.tsx
@@ -5,12 +5,28 @@ const styles = StyleSheet.create({
     position: 'absolute',
     zIndex: 0,
     bottom: 0,
-    paddingLeft: 40,
-    paddingRight: 40,
-    paddingTop: 20,
+  },
+  androidCover: {
+    width: 80,
+    position: 'absolute',
+    top: 0,
+    bottom: 0
+  },
+  iosNavBlurViewSpacing: {
     paddingBottom: 32,
+    paddingTop: 20,
     left: -24,
     right: -24,
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  androidNavBlurViewSpacing: {
+    paddingBottom: 20,
+    paddingTop: 20,
+    left: -64,
+    right: -64,
+    paddingLeft: 84,
+    paddingRight: 84,
   },
   navBack: {
     position: 'absolute',
@@ -24,7 +40,7 @@ const styles = StyleSheet.create({
     position: 'relative',
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   navItem: {
     alignItems: 'center',
