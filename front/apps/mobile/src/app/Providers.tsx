8f08ac0eb56473ed00c6c42a3551b10b601705af
sc-mitton <scm.mitton@gmail.com>
2024-09-16 12:55:55 -0600

    fix bug with appearance settings framework

--- front/apps/mobile/src/app/Providers.tsx
+++ front/apps/mobile/src/app/Providers.tsx
@@ -7,9 +7,9 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import styles from './styles/providers';
 import { ThemeProvider as RestyleThemeProvider, ModalPickerProvider } from '@ledget/native-ui';
-import { useAppearance, setDeviceMode } from '@features/appearanceSlice';
+import { useAppearance, setDeviceMode, selectUseDeviceAppearance } from '@features/appearanceSlice';
 import store, { persistor } from '@features/store';
-import { useAppDispatch } from '@/hooks';
+import { useAppDispatch, useAppSelector } from '@/hooks';
 import { useEffect } from 'react';
 
 const navTheme = {
@@ -23,12 +23,18 @@ const navTheme = {
 const ThemeProvider = ({ children }: { children: (({ mode }: { mode: 'light' | 'dark' }) => React.ReactNode) | React.ReactNode }) => {
   const { mode } = useAppearance();
   const dispatch = useAppDispatch();
+  const useDeviceAppearance = useAppSelector(selectUseDeviceAppearance);
 
+  // This effect makes sure things like the keyboard match the appearance of the app
   useEffect(() => {
     // Add listener to change the appearance of the app
     const appearanceListener = () => {
       setTimeout(() => {
-        Appearance.setColorScheme(mode);
+        if (!useDeviceAppearance) {
+          Appearance.setColorScheme(mode);
+        } else {
+          Appearance.setColorScheme(null);
+        }
       }, 2000);
     }
 
@@ -39,6 +45,13 @@ const ThemeProvider = ({ children }: { children: (({ mode }: { mode: 'light' | '
     };
   }, [mode]);
 
+  useEffect(() => {
+    if (!useDeviceAppearance) {
+      Appearance.setColorScheme(null);
+    }
+  }, [useDeviceAppearance]);
+
+  // Track the device appearance in the redux store
   useEffect(() => {
     const appearanceListener = () => {
       const newDeviceMode = Appearance.getColorScheme() || "light";
