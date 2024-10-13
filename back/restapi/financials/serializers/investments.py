187f16a1a7577c1fc7cdeb05651183098df6c091
sc-mitton <scm.mitton@gmail.com>
2024-10-12 19:52:14 -0600

    add investments views

--- back/restapi/financials/serializers/investments.py
+++ back/restapi/financials/serializers/investments.py
@@ -1,35 +1,65 @@
 from rest_framework import serializers
 
 
+class SecuritySerializer(serializers.Serializer):
+    name = serializers.CharField()
+    ticker_symbol = serializers.CharField()
+    security_id = serializers.CharField()
+
+
+class HoldingSerializer(serializers.Serializer):
+    cost_basis = serializers.FloatField()
+    institution_price = serializers.FloatField()
+    institution_value = serializers.FloatField()
+    quantity = serializers.FloatField()
+    vested_quantity = serializers.FloatField()
+    vested_value = serializers.FloatField()
+    security_id = serializers.CharField()
+
+
+class InvestmentsTransactionSerializer(serializers.Serializer):
+    amount = serializers.FloatField()
+    price = serializers.FloatField()
+    quantity = serializers.FloatField()
+    type = serializers.CharField()
+    subtype = serializers.CharField()
+    date = serializers.DateField()
+    fees = serializers.FloatField()
+    name = serializers.CharField()
+    security_id = serializers.CharField()
+
+
 class InvestmentsSerializer(serializers.Serializer):
-    pass
+    account_id = serializers.IntegerField()
+    holdings = HoldingSerializer(many=True)
+    transactions = InvestmentsTransactionSerializer(many=True)
+    securities = SecuritySerializer(many=True)
+    balance = serializers.FloatField()
 
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
 
-'''
-Data I need returned:
+        transactions = repr.pop('transactions')
+        holdings = repr.pop('holdings')
+        securities = repr.pop('securities')
+        securities_dict = {
+            security['security_id']: security
+            for security in securities
+        }
 
-- balance for each investment account
-- Holdings for each investment account
-- Balance History for each investment account
-
-[
-    {
-        account_id: number,
-        balance_history: [
-            { date: string, balance: number }
-            ...
-        ],
-        holdings: [
-            { name: string, quantity: number, value: number }
-            ...
+        repr['transactions'] = [
+            {
+                **transaction,
+                'security': securities_dict[transaction['security_id']]
+            }
+            for transaction in transactions
         ]
-    }
-]
+        repr['holdings'] = [
+            {
+                **holding,
+                'security': securities_dict[holding['security_id']]
+            }
+            for holding in holdings
+        ]
 
-'''
-request = InvestmentsTransactionsGetRequest(
-    access_token=access_token,
-    start_date=date.fromisoformat('2024-03-01'),
-    end_date=date.fromisoformat('2024-10-01'),
-    options=InvestmentsTransactionsGetRequestOptions()
-)
+        return repr
--- back/restapi/financials/views/investments.py
+++ back/restapi/financials/views/investments.py
@@ -1,3 +1,7 @@
+from itertools import groupby
+from datetime import timedelta
+import json
+
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from django.db.models import Prefetch
@@ -13,7 +17,6 @@ from plaid.model.investment_holdings_get_request_options import (
 )
 from plaid.model_utils import date
 import plaid
-import json
 
 from financials.serializers.investments import InvestmentsSerializer
 from financials.models import PlaidItem, Account
@@ -26,48 +29,95 @@ plaid_client = create_plaid_client()
 class InvestmentsView(GenericAPIView):
     permission_classes = [IsAuthedVerifiedSubscriber]
 
-    def list(self, request):
+    def get(self, request, *args, **kwargs):
 
         prefetch = Prefetch(
             'accounts',
             queryset=Account.objects.filter(
                 type__in=['investment']))
-
         plaid_items = PlaidItem.objects.filter(
+                investments_supported=True,
                 user__in=request.user.account.users.all()) \
             .prefetch_related(prefetch)
 
+        results = []
+        for item in plaid_items:
+            try:
+                plaid_data = self._get_plaid_transactions_data(
+                    item,
+                    kwargs.get('start', date.today()) - timedelta(days=30),
+                    kwargs.get('end', date.today())
+                )
+            except plaid.ApiException as e:
+                if json.loads(e.body)['error_code'] == 'PRODUCT_NOT_SUPPORTED':
+                    continue
+                else:
+                    raise e
+            else:
+                results.extend(plaid_data)
+
         return Response()
 
-    def _get_plaid_holdings_data(self, plaid_item):
+    def _get_plaid_transactions_data(self, plaid_item, start, end):
 
-        try:
-            account_ids = [a.id for a in plaid_item.accounts.all()]
-            request = InvestmentsHoldingsGetRequest(
+        account_ids = [a.id for a in plaid_item.accounts.all()]
+        holdings_request = InvestmentsHoldingsGetRequest(
+            access_token=plaid_item.access_token,
+            options=InvestmentHoldingsGetRequestOptions(
+                account_ids=account_ids))
+        holdings_response = plaid_client.investments_holdings_get(holdings_request)
+
+        holdings = groupby(
+            holdings_response.to_dict()['holdings'], lambda x: x['account_id'])
+        balances = holdings_response.to_dict()['accounts']
+        transactions = self._get_transactions_plaid_data(plaid_item, start, end)
+
+        serialized_data = []
+        for (account_id, transaction_group), (_, holding_group), balances \
+                in zip(transactions, holdings, balances):
+
+            serializer = InvestmentsSerializer(
+                data={
+                    'account_id': account_id,
+                    'holdings': list(holding_group),
+                    'transactions': list(transaction_group),
+                    'securities': holdings_response.to_dict()['securities'],
+                    'balance': balances['balances']['current']
+                }
+            )
+            serializer.is_valid(raise_exception=True)
+            serialized_data.append(serializer.data)
+
+        return serialized_data
+
+    def _get_transactions_plaid_data(plaid_item, start, end):
+        '''
+        Concatenates all of the transactions for the paginated responses
+        '''
+
+        account_ids = [a.id for a in plaid_item.accounts.all()]
+
+        transactions_request = InvestmentsTransactionsGetRequest(
+            access_token=plaid_item.access_token,
+            start_date=start,
+            end_date=end,
+            options=InvestmentsTransactionsGetRequestOptions(
+                account_ids=account_ids))
+        response = plaid_client.investments_transactions_get(
+            transactions_request)
+        investment_transactions = response['investment_transactions']
+
+        transactions = response['investment_transactions']
+
+        while len(investment_transactions) < response['total_investment_transactions']:
+            transactions_request = InvestmentsTransactionsGetRequest(
                 access_token=plaid_item.access_token,
-                options=InvestmentHoldingsGetRequestOptions(account_ids=account_ids))
-            response = plaid_client.investments_holdings_get(request)
-
-        except plaid.ApiException as e:
-            pass
-
-    def _get_plaid_transactions_data(self, plaid_item):
-
-        try:
-            account_ids = [a.id for a in plaid_item.accounts.all()]
-            request = InvestmentsTransactionsGetRequest(
-                access_token=plaid_item.access_token,
+                start_date=start,
+                end_date=end,
                 options=InvestmentsTransactionsGetRequestOptions(
                     account_ids=account_ids))
-        except plaid.ApiException as e:
-            pass
+            response = plaid_client.investments_transactions_get(
+                transactions_request)
+            transactions.extend(response['investment_transactions'])
 
-
-# request = InvestmentsTransactionsGetRequest(
-#     access_token=p.access_token,
-#     start_date=date.fromisoformat('2023-03-01'),
-#     end_date=date.fromisoformat('2024-04-30'),
-#     options=InvestmentsTransactionsGetRequestOptions(
-#         account_ids=[p.accounts.all()[0].id]
-#     )
-# )
+        return groupby(transactions, lambda x: x['account_id'])
