7a2da2d6ede6909a905a9d6a6269c2bd67e50c81
sc-mitton <scm.mitton@gmail.com>
2024-10-12 19:48:43 -0600

    update fixtures dump script to update pks when needed in response objects

--- back/restapi/restapi/test_data/fixtures_dump.sh
+++ back/restapi/restapi/test_data/fixtures_dump.sh
@@ -12,3 +12,21 @@ python ../../manage.py dumpdata financials.UserAccount --indent 4 > ./user_accou
 python ../../manage.py dumpdata budget.UserCategory --indent 4 > ./user_category_fixture.json
 python ../../manage.py dumpdata budget.userBill --indent 4 > ./user_bill_fixture.json
 python ../../manage.py dumpdata core.Settings --indent 4 > ./settings_fixture.json
+
+
+# Use sed to replace all account ids
+old_account_id=$(sed -n 's/.*"account_id": *"\([^"]*\)".*/\1/p' ./responses/plaid_sync_response.json | head -1)
+first_account_id=$(sed -n 's/.*"pk": *"\([^"]*\)".*/\1/p' account_fixture.json | head -1)
+sed -i "s/<${old_item_id}>/${first_account_id}/g" ./responses/plaid_sync_response.json
+
+# Use sed to replace all the item ids
+
+old_item_id=$(sed -n 's/.*"item_id": *"\([^"]*\)".*/\1/p' ./plaid_webhook_objects/error.json | head -1)
+first_item_id=$(sed -n 's/.*"pk": *"\([^"]*\)".*/\1/p' plaid_item_fixture.json | head -1)
+
+sed -i "s/<${old_item_id}>/${first_item_id}/g" ./plaid_webhook_objects/error.json
+sed -i "s/<${old_item_id}>/${first_item_id}/g" ./plaid_webhook_objects/login_repaired.json
+sed -i "s/<${old_item_id}>/${first_item_id}/g" ./plaid_webhook_objects/new_accounts_available.json
+sed -i "s/<${old_item_id}>/${first_item_id}/g" ./plaid_webhook_objects/pending_expiration.json
+sed -i "s/<${old_item_id}>/${first_item_id}/g" ./plaid_webhook_objects/sync_updates_available.json
+sed -i "s/<${old_item_id}>/${first_item_id}/g" ./plaid_webhook_objects/user_permission_revoked.json
