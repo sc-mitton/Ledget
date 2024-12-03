c33251343752afa92b6dcb7a900c28729e7b464f
sc-mitton <scm.mitton@gmail.com>
2024-12-02 18:42:19 -0700

    dead code removal

--- front/apps/mobile/src/screens/Budget/bills/Bills.tsx
+++ front/apps/mobile/src/screens/Budget/bills/Bills.tsx
@@ -9,10 +9,8 @@ import { BudgetScreenProps } from '@types';
 import List from './List';
 import Calendar from './Calendar';
 import { useBudgetContext } from '../context';
-import { useLoaded } from '@ledget/helpers';
 
 const Bills = (props: BudgetScreenProps<'Main'>) => {
-  const loaded = useLoaded();
   const [index, setIndex] = useState(0);
   const ref = useRef<TPagerViewRef>(null);
   const { setBillsIndex } = useBudgetContext();
