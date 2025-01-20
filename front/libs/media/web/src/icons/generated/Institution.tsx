c27830a13a7ec541532c3928da7de468fb5d0b98
sc-mitton <scm.mitton@gmail.com>
2025-01-20 11:30:36 -0700

    minor style updates

--- front/libs/media/web/src/icons/generated/Institution.tsx
+++ front/libs/media/web/src/icons/generated/Institution.tsx
@@ -23,7 +23,6 @@ function SvgInstitution({
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={1.5}
         d="M9.8 11.9v10.3M20.6 11.9v10.3"
       />
       <path
@@ -31,7 +30,6 @@ function SvgInstitution({
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={1.398}
         d="M4.4 11.9v10.3M12.2 2.9 3 6.8c-1 .3-.8 1.9.3 1.9h18.4c1.1 0 1.3-1.5.3-1.9L12.9 3c-.3-.3-.5-.3-.7-.1"
       />
       <path
@@ -39,7 +37,6 @@ function SvgInstitution({
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={1.5}
         d="M2.3 22.2h20.4M15.2 11.9v10.3"
       />
     </svg>
