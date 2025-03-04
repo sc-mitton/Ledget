d8bed2e25ae7dce276e553559154ba18a53060e8
sc-mitton <scm.mitton@gmail.com>
2025-03-04 15:19:58 -0700

    fix certbox hook and security group ingress for eb-restapi

--- back/restapi/.platform/hooks/prebuild/nginx_certs_setup.sh
+++ back/restapi/.platform/hooks/prebuild/nginx_certs_setup.sh
@@ -7,9 +7,7 @@ bucket_path="${bucket}/letsencrypt"
 
 # Add cron job
 function add_cron_job {
-    touch /etc/cron.d/certbot_renew
-    echo "* * * * * webapp 0 2 * * * certbot renew --allow-subset-of-names
-    # empty line" | tee /etc/cron.d/certbot_renew
+    echo "0 2 * * * sudo certbot renew" | sudo crontab -u webapp -
 }
 
 #check if certbot is already installed
@@ -18,7 +16,7 @@ if command -v certbot &>/dev/null; then
 else
     # Install certbot and plugin since it's not installed already
     # Instructions from https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/SSL-on-amazon-linux-2.html#letsencrypt
-
+    echo "Installing certbot and plugins"
     sudo dnf install -y python3-certbot-nginx
     sudo dnf install -y python3-certbot-dns-route53
 fi
@@ -38,12 +36,25 @@ if [ -n "$(aws s3 ls $bucket_path)" ]; then
     # --nginx: Automatically configures Nginx with the certificate.
     # --agree-tos: Automatically agrees to the terms of service.
     # --email ${contact}: Sets the email for renewal notifications.
-    # --reinstall: Reinstalls an existing certificate.
+    # --reinstall: If the requested certificate matches an existing
+    #      certificate, always keep the existing one until it is
+    #      due for renewal
     # --redirect: Forces HTTPS.
     # --expand: Adds domains to an existing certificate.
     # --allow-subset-of-names: Issues a certificate even if some domains fail validation.
-    sudo certbot -n -d ${domain} --nginx --agree-tos --email ${contact} --reinstall --redirect --expand --allow-subset-of-names
-    systemctl reload nginx
+    sudo certbot \
+        -n \
+        -d ${domain} \
+        --dns-route53 \
+        --nginx \
+        --agree-tos \
+        --email ${contact} \
+        --reinstall \
+        --redirect \
+        --expand \
+        --allow-subset-of-names
+
+    systemctl reload k
 
     # re-uploading the certificate in case of renewal during certbot installation
     tar -czvf /tmp/backup.tar.gz /etc/letsencrypt/*
@@ -54,8 +65,20 @@ if [ -n "$(aws s3 ls $bucket_path)" ]; then
 fi
 
 # obtain, install, and upload certificate to S3 bucket since it does not exist already
-sudo certbot certonly -n -d $domain --preferred-challenges dns --dns-route53 --email ${contact} --agree-tos
+echo "Obtaining certificate"
+sudo certbot certonly \
+    -n \
+    -d ${domain} \
+    --dns-route53 \
+    --nginx \
+    --email ${contact} \
+    --agree-tos \
+    --redirect \
+    --allow-subset-of-names
+
+echo "Uploading certificate to S3 bucket"
 tar -czvf /tmp/backup.tar.gz /etc/letsencrypt/*
 aws s3 cp /tmp/backup.tar.gz ${bucket_path}/backup.tar.gz
 
+echo "Adding cron job"
 add_cron_job
