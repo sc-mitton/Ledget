be492ecc30aad111b1fe6c00821df0001b224481
sc-mitton <scm.mitton@gmail.com>
2025-03-04 19:16:55 -0700

    add cors settings to dev

--- back/restapi/restapi/settings/dev.py
+++ back/restapi/restapi/settings/dev.py
@@ -7,6 +7,7 @@ import json
 import sys
 from pathlib import Path
 
+from corsheaders.defaults import default_headers
 
 # IMPORTANT FLAGS
 DEVELOPMENT = True
@@ -26,7 +27,30 @@ MEDIA_ROOT = '/restapi/media'
 CSRF_TRUSTED_ORIGINS = ['https://localhost:3000', 'https://localhost:3001']
 CSRF_COOKIE_DOMAIN = 'localhost'
 
+# ----------------------------------- CORS ----------------------------------- #
+
+CORS_ALLOWED_ORIGINS = [
+    'https://localhost:3000',
+    'https://localhost:3001',
+]
+
+CORS_ALLOWED_HEADERS = [
+    *default_headers,
+    'x-forwarded-host',
+    'x-user',
+    'x-device-token',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+INSTALLED_APPS += [
+    'corsheaders',
+]
+
+MIDDLEWARE.insert(1, 'corsheaders.middleware.CorsMiddleware')
+
 # ---------------------------------- 3rd Party Services --------------------------------- #
+
 
 def get_secret(secret):
     try:
@@ -34,6 +58,7 @@ def get_secret(secret):
             return f.read().strip()
     except FileNotFoundError:
         return ' '
+
 
 # Django
 SECRET_KEY = get_secret('django_secret_key')
@@ -57,7 +82,7 @@ ORY_ACTIVATION_REDIRECT_URL = f'https://{ACCOUNTS_APP_DOMAIN}/activation'
 # Oathkeeper
 OATHKEEPER_ENDPOINT = 'http://oathkeeper:4456/.well-known/jwks.json'
 if any(a in sys.argv for a in ['test', 'test_coverage', 'migrate', 'makemigrations']):
-    OATHKEEPER_PUBLIC_KEY=None
+    OATHKEEPER_PUBLIC_KEY = None
 else:
     jwks = requests.get(OATHKEEPER_ENDPOINT).json()['keys']
     OATHKEEPER_PUBLIC_KEY = jwt.algorithms.RSAAlgorithm.from_jwk(
@@ -68,13 +93,15 @@ SPARKPOST_API_KEY = get_secret('sparkpost_api_key')
 
 # Plaid
 PLAID_ENVIRONMENT = os.getenv('PLAID_ENVIRONMENT') or 'Sandbox'
-PLAID_API_KEY = get_secret('plaid_sand_api_key') if PLAID_ENVIRONMENT.lower() == 'sandbox' else get_secret('plaid_api_key')
+PLAID_API_KEY = get_secret('plaid_sand_api_key') if PLAID_ENVIRONMENT.lower(
+) == 'sandbox' else get_secret('plaid_api_key')
 PLAID_CLIENT_ID = get_secret('plaid_client_id')
 PLAID_REDIRECT_URI_ONBOARDING = 'https://localhost:3000/welcome/connect'
 PLAID_REDIRECT_URI = 'https://localhost:3000/settings/connections'
 
 # This webhook endpoint wont work since we have to use ngrok, but it has to be something for the code to work
-PLAID_WEBHOOK_ENDPOINT = os.getenv('PLAID_WEBHOOK_ENDPOINT') or 'https://localhost:8000/hooks/plaid/item'
+PLAID_WEBHOOK_ENDPOINT = os.getenv(
+    'PLAID_WEBHOOK_ENDPOINT') or 'https://localhost:8000/hooks/plaid/item'
 
 # --------------------------------- Postgres --------------------------------- #
 
